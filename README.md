20ce1736eb56fd1a3c24694e9a9036ed97ddc0dc
Mohamad Al Sayadi <mohamad.alsayadi@alefeducation.com>
2021-06-19 21:59:50 +0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This repository has the codebase for my Medium article: https://medium.com/@Sayadi/how-to-integrate-elastic-apm-java-agent-with-spring-boot
---
+++ src/main/java/io/sayadi/elasticapmspringbootintegration/ElasticApmSpringBootIntegrationApplication.java
@@ -0,0 +1,12 @@
+package io.sayadi.elasticapmspringbootintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticApmSpringBootIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticApmSpringBootIntegrationApplication.class, args);
+	}
+}
